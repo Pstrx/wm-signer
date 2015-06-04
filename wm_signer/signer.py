f0c196e2b08308405b2ce348aa47da2b18f97529
Egor Smolyakov <egorsmkv@gmail.com>
2015-06-04 23:11:32 +0300

    Fix comment

--- wm_signer/signer.py
+++ wm_signer/signer.py
@@ -145,8 +145,7 @@ class Signer:
     @staticmethod
     def reverse_to_decimal(value):
         """
-        Обратный порядок байтов с преобразованием двоичных данных
-        в десятичную форму.
+        Преобразованием двоичных данных в десятичную форму.
         """
 
         return int.from_bytes(value, byteorder='little')
